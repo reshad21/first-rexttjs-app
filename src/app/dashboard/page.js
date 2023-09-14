09b700a31a71b8fdfe5ff7a600780cf233a628a4
reshad21 <miakala7@gmail.com>
2023-09-14 22:15:49 +0600

    route and nested route create

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,10 @@
+
+const DashbordPage = () => {
+    return (
+        <div className='text-blue-700 text-xl'>
+            Welcome to Our dashbord
+        </div>
+    );
+};
+
+export default DashbordPage;
